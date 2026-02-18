ec5f45aa4472926c46be3011421fd3de198423bd
jude <camirisner22@gmail.com>
2026-02-18 15:10:55 +0100

    Initial commit

---
+++ accueil skornnen/src/page/index.tsx
@@ -0,0 +1,462 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <div className="frame">
+        <div className="frame-1">
+          <div className="header">
+            <div className="frame-2">
+              <div className="skornenn" />
+            </div>
+          </div>
+        </div>
+        <div className="frame-3">
+          <div className="header-4" />
+          <div className="frame-5">
+            <div className="frame-6">
+              <div className="frame-7">
+                <div className="frame-8">
+                  <div className="line" />
+                  <span className="nuances-de-ciders">6 NUANCES DE CIDERS</span>
+                  <div className="line-9" />
+                </div>
+                <div className="frame-a">
+                  <div className="product-card-test-animation-v" />
+                  <div className="product-card-test-animation-v-b" />
+                  <div className="product-card-test-animation-v-c" />
+                  <div className="product-card-test-animation-v-d" />
+                  <div className="product-card-test-animation-v-e" />
+                  <div className="product-card-test-animation-v-f" />
+                </div>
+                <div className="frame-10">
+                  <div className="component">
+                    <div className="frame-11">
+                      <div className="line-12" />
+                    </div>
+                    <div className="frame-13">
+                      <span className="en-voir-plus">En voir plus</span>
+                      <div className="frame-14" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-15">
+          <div className="frame-16" />
+          <div className="frame-17">
+            <div className="frame-18">
+              <div className="vector" />
+            </div>
+            <div className="frame-19">
+              <div className="frame-1a">
+                <div className="frame-1b">
+                  <span className="le-cider-francais-debarque-enfin-en-france">
+                    Le cider Francais débarque enfin en France 
+                  </span>
+                  <span className="le-hard-cider">
+                    Le Hard Cider, c’est l’incontournable des pubs et terrasses
+                    anglo-saxons ! Il débarque enfin en France sous le nom de La
+                    Mordue, pour vous offrir une expérience fruitée et très
+                    rafraichissante !<br />
+                    <br />
+                    Le Hard Cider, c’est cette boisson à base d’alcool peu
+                    calorique, composée de pommes qui cartonne dans les pays
+                    anglo-saxons depuis quelques années. Une boisson fraîche,
+                    fruitée et légèrement acidulée, sans amertume,
+                    incontournable à l’étranger. Impossible de passer à côté...
+                    <br />
+                    <br />
+                    Après nous être demandé par quoi remplacer la bière, nous
+                    avons fait le pari un peu fou de lancer notre Hard Cider. Un
+                    hard Cider français, avec une recette originale, des pommes
+                    choisies rigoureusement, une fabrication unique, et une
+                    mission : faire découvrir et adopter la tendance Hard Cider
+                    en France. C'est le début de l'histoire de La Mordue.
+                    <br />
+                  </span>
+                </div>
+                <div className="button">
+                  <span className="nos-produits">Découvrir notre histoire</span>
+                </div>
+              </div>
+              <div className="frame-1c" />
+            </div>
+            <div className="frame-1d">
+              <div className="vector-1e" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="frame-1f">
+        <div className="frame-20">
+          <div className="frame-21">
+            <div className="mingcute-lock-fill" />
+          </div>
+          <div className="frame-22">
+            <span className="www-figma-com-test-interface-simulation-ui">
+              www.figma.com/test/interface_simulation/ui
+            </span>
+          </div>
+          <div className="frame-23">
+            <div className="lets-icons-send-hor" />
+          </div>
+        </div>
+        <div className="frame-24">
+          <div className="frame-25">
+            <div className="skornenn-26" />
+          </div>
+          <div className="frame-27">
+            <div className="frame-28">
+              <div className="component-29">
+                <div className="skornenn-icon" />
+                <div className="frame-2a">
+                  <div className="line-2b" />
+                  <span className="recherche">Recherche</span>
+                </div>
+              </div>
+              <div className="component-2c">
+                <div className="skornenn-icon-2d" />
+                <div className="frame-2e">
+                  <div className="line-2f" />
+                  <span className="profil">Profil</span>
+                </div>
+              </div>
+              <div className="component-30">
+                <div className="skornenn-icon-31" />
+                <div className="frame-32">
+                  <div className="line-33" />
+                  <span className="panier">Panier</span>
+                </div>
+              </div>
+            </div>
+            <div className="menu-icon-large">
+              <div className="frame-34" />
+              <div className="line-35">
+                <div className="menu" />
+                <span className="top-scroll-button">Menu</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="frame-36">
+        <div className="frame-37" />
+      </div>
+      <div className="frame-38">
+        <div className="frame-39">
+          <div className="frame-3a">
+            <div className="frame-3b">
+              <div className="frame-3c">
+                <div className="frame-3d">
+                  <div className="skornenn-le-cider-de-caractere">
+                    <span className="au-coeur-de-la-bretagne">
+                      Skornenn, le cider de caractère
+                    </span>
+                    <span className="nuances-de-cidres">
+                      Au cœur de la Bretagne, terre de légendes et de
+                      traditions, la distillerie Skornenn se distingue par son
+                      engagement à produire des cidres d'exception. Fondée par
+                      des passionnés du terroir breton, Skornenn allie
+                      savoir-faire ancestral et innovation moderne pour offrir
+                      une gamme de cidres doux et parfumés, disponibles avec et
+                      sans alcool, en canette et en bouteille.
+                      <br />
+                      <br />
+                      Les vergers de Skornenn, situés dans les vallons
+                      verdoyants de la Bretagne, bénéficient d'un climat tempéré
+                      idéal pour la culture des pommes à cidre. Chaque pomme est
+                      sélectionnée pour sa qualité et son goût unique,
+                      garantissant ainsi un cidre authentique et savoureux.
+                      <br />
+                      <br />À Skornenn, la production du cidre est un véritable
+                      art. Les pommes, récoltées à la main, sont pressées selon
+                      des méthodes traditionnelles pour en extraire le meilleur
+                      jus. La fermentation est réalisée lentement, permettant de
+                      développer des arômes riches et complexes. Cette attention
+                      portée à chaque étape de la production assure un cidre
+                      doux et parfumé, reflet du terroir breton.
+                      <br />
+                      <br />6 NUANCES DE CIDRES
+                    </span>
+                  </div>
+                  <div className="frame-3e">
+                    <div className="frame-3f" />
+                  </div>
+                </div>
+              </div>
+              <div className="pub-slider">
+                <div className="vector-40" />
+                <div className="frame-41">
+                  <div className="slider-arrow">
+                    <div className="frame-42">
+                      <div className="frame-43" />
+                    </div>
+                  </div>
+                  <div className="slider-arrow-44" />
+                </div>
+                <div className="frame-45">
+                  <div className="pause-play">
+                    <div className="group" />
+                  </div>
+                  <div className="frame-46">
+                    <div className="rectangle" />
+                  </div>
+                  <div className="frame-47">
+                    <div className="rectangle-48" />
+                  </div>
+                  <div className="frame-49">
+                    <div className="rectangle-4a" />
+                  </div>
+                  <div className="frame-4b">
+                    <div className="rectangle-4c" />
+                  </div>
+                </div>
+                <div className="frame-4d">
+                  <div className="frame-4e" />
+                  <div className="frame-4f" />
+                  <div className="frame-50" />
+                  <div className="frame-51" />
+                  <div className="frame-52" />
+                  <div className="frame-53" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-54">
+          <div className="frame-55" />
+          <div className="frame-56">
+            <div className="frame-57">
+              <div className="frame-58">
+                <div className="line-59" />
+                <span className="nuances-de-ciders-5a">
+                  6 NUANCES DE CIDERS
+                </span>
+                <div className="line-5b" />
+              </div>
+              <div className="frame-5c">
+                <div className="frame-5d">
+                  <div className="product-card-test" />
+                  <div className="product-card-test-5e" />
+                  <div className="product-card-test-5f" />
+                  <div className="product-card-test-60" />
+                  <div className="product-card-test-61" />
+                  <div className="product-card-test-62" />
+                </div>
+                <div className="frame-63">
+                  <div className="component-64">
+                    <div className="frame-65">
+                      <div className="line-66" />
+                    </div>
+                    <div className="frame-67">
+                      <span className="en-voir-plus-68">En voir plus</span>
+                      <div className="frame-69" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="vector-6a" />
+          </div>
+        </div>
+      </div>
+      <div className="frame-6b">
+        <div className="frame-6c">
+          <div className="frame-6d">
+            <div className="component-6e">
+              <div className="frame-6f">
+                <div className="frame-70">
+                  <div className="frame-71">
+                    <span className="cidre-parfume">ORANGE SANGUINE </span>
+                    <div className="button-72">
+                      <span className="nos-produits-73">
+                        Découvrir la sélection
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="component-74">
+              <div className="frame-75">
+                <div className="frame-76">
+                  <div className="frame-77">
+                    <span className="cidre-parfume-78">POIRE </span>
+                    <div className="button-79">
+                      <span className="nos-produits-7a">
+                        Découvrir la sélection
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="component-7b">
+            <div className="frame-7c">
+              <div className="frame-7d">
+                <div className="frame-7e">
+                  <span className="cidre-parfume-7f">
+                    L’ORIGINALE / AUTHENTIQUE{" "}
+                  </span>
+                  <div className="button-80">
+                    <span className="nos-produits-81">
+                      Découvrir la sélection
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-82">
+            <div className="component-83">
+              <div className="frame-84">
+                <div className="frame-85">
+                  <div className="frame-86">
+                    <span className="cidre-parfume-87">
+                      FRUIT DE LA PASSION
+                    </span>
+                    <div className="button-88">
+                      <span className="nos-produits-89">
+                        Découvrir la sélection
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="component-8a">
+              <div className="frame-8b">
+                <div className="frame-8c">
+                  <div className="frame-8d">
+                    <span className="cidre-parfume-8e">FRUITS ROUGES </span>
+                    <div className="button-8f">
+                      <span className="nos-produits-90">
+                        Découvrir la sélection
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="frame-91">
+        <div className="frame-92">
+          <div className="frame-93">
+            <div className="frame-94">
+              <div className="frame-95">
+                <div className="skornenn-96" />
+                <div className="frame-97">
+                  <span className="copyright">© Skornenn 2024</span>
+                  <span className="all-right-reserved">
+                    - all right reserved
+                  </span>
+                </div>
+              </div>
+              <div className="frame-98">
+                <div className="link">
+                  <div className="frame-99">
+                    <span className="lien">Cidres parfumés</span>
+                  </div>
+                  <div className="line-9a" />
+                </div>
+                <div className="frame-9b">
+                  <div className="frame-9c">
+                    <span className="lien-9d">Cidres nature</span>
+                  </div>
+                  <div className="line-9e" />
+                </div>
+                <div className="frame-9f">
+                  <div className="frame-a0">
+                    <span className="lien-a1">Cidres BIO</span>
+                  </div>
+                  <div className="line-a2" />
+                </div>
+                <div className="frame-a3">
+                  <div className="frame-a4">
+                    <span className="cidres-sans-alcool">
+                      Cidres sans alcool
+                    </span>
+                  </div>
+                  <div className="line-a5" />
+                </div>
+                <div className="frame-a6">
+                  <div className="frame-a7">
+                    <span className="cidres-glace">Cidres glace</span>
+                  </div>
+                  <div className="line-a8" />
+                </div>
+              </div>
+            </div>
+            <div className="frame-a9">
+              <div className="social-network-link">
+                <div className="social-network-icon">
+                  <div className="component-aa" />
+                </div>
+                <div className="frame-ab">
+                  <span className="intagram">Intagram</span>
+                </div>
+              </div>
+              <div className="social-network-link-ac">
+                <div className="social-network-icon-ad">
+                  <div className="component-ae" />
+                </div>
+                <div className="frame-af">
+                  <span className="facebook">Facebook</span>
+                </div>
+              </div>
+              <div className="social-network-link-b0">
+                <div className="social-network-icon-b1">
+                  <div className="component-b2" />
+                </div>
+                <div className="frame-b3">
+                  <span className="x-twitter">X / Twitter</span>
+                </div>
+              </div>
+              <div className="social-network-link-b4">
+                <div className="social-network-icon-b5">
+                  <div className="component-b6" />
+                </div>
+                <div className="frame-b7">
+                  <span className="linkedin">Linkedin</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-b8">
+            <div className="frame-b9">
+              <div className="footer-secondary-link">
+                <span className="cgu">CGU</span>
+                <div className="line-ba" />
+              </div>
+              <div className="footer-secondary-link-bb">
+                <span className="cgv">CGV</span>
+                <div className="line-bc" />
+              </div>
+              <div className="footer-secondary-link-bd">
+                <span className="politique-de-confidentialite">
+                  Politique de confidentialité
+                </span>
+                <div className="line-be" />
+              </div>
+              <div className="footer-secondary-link-bf">
+                <span className="mentions-legales">Mentions légales</span>
+                <div className="line-c0" />
+              </div>
+              <div className="footer-secondary-link-c1">
+                <span className="contacts">Contacts</span>
+                <div className="line-c2" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
